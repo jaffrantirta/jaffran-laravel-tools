fcea3a033c4bb06d0f8212830b9666600a0541ea
jaffrantirta <jaffrantirta@icloud.com>
2024-04-01 22:45:19 +0800

    update unum

--- src/Actions/GenerateFileFromStubAction.php
+++ src/Actions/GenerateFileFromStubAction.php
@@ -9,13 +9,13 @@ class GenerateFileFromStubAction
 {
     public function execute(string $stub, string $filenameSuffix, string $fileLocation, string $name): void
     {
-        $className = ucfirst($name) . $filenameSuffix;
+        $className = ucfirst($name);
 
         $stubPath = dirname(__DIR__, 2) . '/stubs/';
         $content = File::get($stubPath . "$stub.stub");
 
-        $content = str_replace('{name}', Str::lower($name), $content);
-        $content = str_replace('{Name}', ucfirst($name), $content);
+        $content = str_replace('{name}', Str::lower($className), $content);
+        $content = str_replace('{Name}', ucfirst($className), $content);
 
         File::ensureDirectoryExists(app_path($fileLocation));
         File::put(app_path("$fileLocation" . $className . '.php'), $content);
